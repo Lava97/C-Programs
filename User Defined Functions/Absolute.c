751260b00046fc9db12bf3c97fb1d3601ec6cd10
Lalit Chandwani <lalitdc97@gmail.com>
2017-10-28 17:01:51 +0530

    Create Absolute.c

---
+++ User Defined Functions/Absolute.c
@@ -0,0 +1,25 @@
+//This file contains only the definitions of the functions.
+
+int ABS(int number){
+  return number<0 ? -number : number;
+}
+
+long int LABS(long number){
+  return number<0 ? -number : number;
+}
+
+long long int LLABS(long long number){
+  return number<0 ? -number : number;
+}
+
+float FABSF(float number){
+  return number<0 ? -number : number;
+}
+
+double FABS(double number){
+  return number<0 ? -number : number;
+}
+
+long double FABSL(long double number){
+  return number<0 ? -number : number;
+}
